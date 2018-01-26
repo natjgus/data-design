793acb17512df14e5a00a0a684416ac2b4da5d24
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-26 09:58:02 -0700

    finished clap PDO

--- php/classes/Clap.php
+++ php/classes/Clap.php
@@ -20,7 +20,7 @@ class Clap implements \JsonSerializable {
 	use ValidateDate;
 	use ValidateUuid;
 	/** id of the clap; this is the parent key
-	* @var Uuid $clapId
+	 * @var Uuid $clapId
 	 */
 	private $clapId;
 
@@ -44,9 +44,10 @@ class Clap implements \JsonSerializable {
 	 *
 	 * @return Uuid value of clap  id
 	 **/
-	public function getClapId() : Uuid{
-		return($this->clapId);
+	public function getClapId(): Uuid {
+		return ($this->clapId);
 	}
+
 	/**
 	 * mutator method for clap id
 	 *
@@ -54,7 +55,7 @@ class Clap implements \JsonSerializable {
 	 * @throws \RangeException if $newClapId is not positive
 	 * @throws \TypeError if $newClapId is not an integer
 	 **/
-	public function setClapId($newClapId) : void {
+	public function setClapId($newClapId): void {
 		try {
 			$uuid = self::validateUuid($newClapId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -64,14 +65,16 @@ class Clap implements \JsonSerializable {
 		// convert and store the clap id
 		$this->clapId = $uuid;
 	}
+
 	/**
 	 * accessor method for clap article id
 	 *
 	 * @return Uuid value of clap article id
 	 **/
-	public function getClapArticleId() : Uuid {
-		return($this->clapArticleId);
+	public function getClapArticleId(): Uuid {
+		return ($this->clapArticleId);
 	}
+
 	/**
 	 * mutator method for clap article id
 	 *
@@ -79,7 +82,7 @@ class Clap implements \JsonSerializable {
 	 * @throws \RangeException if $newClapArticleId is not positive
 	 * @throws \TypeError if $newClapArticleId is not an integer
 	 **/
-	public function setClapArticleId($newClapArticleId) : void {
+	public function setClapArticleId($newClapArticleId): void {
 		try {
 			$uuid = self::validateUuid($newClapArticleId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -89,14 +92,16 @@ class Clap implements \JsonSerializable {
 		// convert and store the profile id
 		$this->clapArticleId = $uuid;
 	}
+
 	/**
 	 * accessor method for clap profile id
 	 *
 	 * @return Uuid value of clap profile id
 	 **/
-	public function getClapProfileId() : Uuid{
-		return($this->clapProfileId);
+	public function getClapProfileId(): Uuid {
+		return ($this->clapProfileId);
 	}
+
 	/**
 	 * mutator method for clap profile id
 	 *
@@ -104,7 +109,7 @@ class Clap implements \JsonSerializable {
 	 * @throws \RangeException if $newClapProfileId is not positive
 	 * @throws \TypeError if $newClapProfileId is not an integer
 	 **/
-	public function setClapProfileId($newClapProfileId) : void {
+	public function setClapProfileId($newClapProfileId): void {
 		try {
 			$uuid = self::validateUuid($newClapProfileId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -114,14 +119,16 @@ class Clap implements \JsonSerializable {
 		// convert and store the profile id
 		$this->clapProfileId = $uuid;
 	}
+
 	/**
 	 * accessor method for clap date
 	 *
 	 * @return \DateTime value of clap date
 	 **/
-	public function getClapDate() : \DateTime {
-		return($this->clapDate);
+	public function getClapDate(): \DateTime {
+		return ($this->clapDate);
 	}
+
 	/**
 	 * mutator method for clap date
 	 *
@@ -129,7 +136,7 @@ class Clap implements \JsonSerializable {
 	 * @throws \InvalidArgumentException if $newClapDate is not a valid object or string
 	 * @throws \RangeException if $newClapDate is a date that does not exist
 	 **/
-	public function setClapDate($newClapDate = null) : void {
+	public function setClapDate($newClapDate = null): void {
 		// base case: if the date is null, use the current date and time
 		if($newClapDate === null) {
 			$this->clapDate = new \DateTime();
@@ -144,13 +151,14 @@ class Clap implements \JsonSerializable {
 		}
 		$this->clapDate = $newClapDate;
 	}
+
 	/**
 	 * inserts this clap into mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @throws \PDOException when mySQL related errors occur
 	 **/
-	public function insert(\PDO $pdo) : void {
+	public function insert(\PDO $pdo): void {
 		//create query template
 		$query = "INSERT INTO clap(clapId, clapArticleId, clapProfileId, clapDate) VALUES(:clapId, :clapArticleId, :clapProfileId, :clapDate)";
 		$statement = $pdo->prepare($query);
@@ -160,32 +168,141 @@ class Clap implements \JsonSerializable {
 		$statement->execute($parameters);
 
 	}
+
 	/**
 	 *deletes clap from mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @throws \PDOException when mySQL related errors occur
 	 **/
-	public function delete(\PDO $pdo) : void {
+	public function delete(\PDO $pdo): void {
 		//create a query template
 		$query = "DELETE FROM clap WHERE clapId = :clapId AND clapArticleId = :clapArticleId AND clapProfileId = :clapProfileId";
 		$statement = $pdo->prepare($query);
 		//bind the member variables to the placeholders in template
-		$parameters = ["clapId" => $this->clapId->getBytes(), "clapArticleId" =>$this->clapArticleId->getBytes(), "clapProfileId" =>$this->clapProfileId->getBytes()];
+		$parameters = ["clapId" => $this->clapId->getBytes(), "clapArticleId" => $this->clapArticleId->getBytes(), "clapProfileId" => $this->clapProfileId->getBytes()];
 		$statement->execute($parameters);
 	}
 
+	/**
+	 * gets the clap by clap id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param string|Uuid $clapProfileId profile id to search for
+	 * @return \SplFixedArray SplFixedArray of claps found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 **/
+	public static function getClapByClapId(\PDO $pdo, $clapId) {
+		//sanitize the clapId before searching
+		try {
+			$clapId = self::validateUuid($clapId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		//create media query template
+		$query = "SELECT clapId, clapProfileId, clapArticleId, articleDate FROM clap WHERE clapId = clapId";
+		$statement = $pdo->prepare($query);
+		//bind the article id to the place holder in the template
+		$parameters = ["clapId" => $clapId->getBytes()];
+		$statement->execute($parameters);
+		// grab the clap from mySQL
+		try {
+			$clap = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$article = new Article($row["clapId"], $row["clapProfileId"], $row["clapArticleId"], $row["clapDate"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($clap);
+	}
+
+	/**
+	 * gets the clap by profile id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param string|Uuid $clapProfileId profile id to search for
+	 * @return \SplFixedArray SplFixedArray of claps found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 **/
+	public static function getClapByClapProfileId(\PDO $pdo, $clapProfileId): \SPLFixedArray {
+		try {
+			$clapProfileId = self::validateUuid($clapProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		// create query template
+		$query = "SELECT clapId, clapArticleId, clapProfileId, clapDate FROM clap WHERE clapProfileId = :clapProfileId";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holders in the template
+		$parameters = ["clapProfileId" => $clapProfileId->getBytes()];
+		$statement->execute($parameters);
+		// build an array of claps
+		$claps = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$clap = new Clap($row["clapId"], $row["clapArticleId"], $row["clapProfileId"], $row["clapDate"]);
+				$claps[$claps->key()] = $clap;
+				$claps->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($claps);
+	}
+
+	/**
+	 * gets the clap by article id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param string|Uuid $clapArticleId article id to search for
+	 * @return \SplFixedArray array of Likes found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 **/
+	public static function getClapByClapArticleId(\PDO $pdo, $clapArticleId): \SplFixedArray {
+		try {
+			$clapArticleId = self::validateUuid($clapArticleId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		// create query template
+		$query = "SELECT clapId, clapArticleId, clapProfileId, clapDate FROM clap WHERE clapArticleId = :clapArticleId";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holders in the template
+		$parameters = ["clapArticleId" => $clapArticleId->getBytes()];
+		$statement->execute($parameters);
+		// build the array of likes
+		$claps = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$clap = new Clap($row["clapId"], $row["clapArticleId"], $row["clapProfileId"], $row["clapDate"]);
+				$claps[$claps->key()] = $clap;
+				$claps->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($claps);
+	}
 
 	/**
 	 * formats the state variables for JSON serialization
 	 *
 	 * @return array resulting state variables to serialize
 	 **/
-	public function jsonSerialize() {
+	public function jsonSerialize(): array {
 		$fields = get_object_vars($this);
-		$fields["profileId"] = $this->clapId->toString();
-		unset($fields["profileHash"]);
-		unset($fields["profileSalt"]);
+		$fields["clapId"] = $this->clapId->toString();
+		$fields["clapArticleId"] = $this->clapArticleId->toString();
+		$fields["clapProfileId"] = $this->clapProfileId->toString();
+		$fields["clapDate"] = round(floatval($this->clapDate->format("U.u")) * 1000);
 		return ($fields);
 	}
 }
